51941453d7cf06e874b2cb9c64467956eadf01a1
Afiful-Ehsan <afifulehsan001@gmail.com>
2026-04-27 00:18:26 +0600

    Add files via upload

---
+++ practice 3.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+
+    int n,i;
+    scanf("%d",&n);
+ int a[n];
+    for (i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+    int index;
+    scanf("%d",&index );
+
+    for(i=index;i<n-1;i++)
+    {
+        a[i]=a[i+1];
+    }
+
+    for(i=0;i<n-1;i++)
+    {
+        printf("%d ",a[i]);
+    }
+
+
+    return 0;
+}
+
